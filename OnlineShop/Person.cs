cb7fdd06be0fd4ee18a9c9c562ce90b152092006
Adriel Linares Ramos <adriellr01@gmail.com>
2023-03-21 21:58:17 -0400

    Add project files.

---
+++ OnlineShop/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace OnlineShop
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Book> Books { get; set; }
+
+        public void AddBook()
+        {
+            Book book = new Book();
+
+            book.Title = "Web programming";
+            book.Description = "Programación web para principantes";
+
+            Books.Add(book);
+        }
+    }
+}

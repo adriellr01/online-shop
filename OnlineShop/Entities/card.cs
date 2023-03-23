1c269e4203bddad06d10b6efced51bb24f3111e3
hp <hp@DESKTOP-OTT32A7>
2023-03-23 23:57:22 +0100

    subir card class

--- OnlineShop/Entities/card.cs
+++ OnlineShop/Entities/card.cs
@@ -8,5 +8,8 @@ public class card
 
 	public int tipoid { get; set; }  //tipo de tarjeta(visa, metropolitano, etc)
 		                             //este tipo es llave foranea 
-}
+									 //hay que crear un nomenclador para ese tipo de tarjeta
+
+		public bool vigente { get; set; }  //saber si la tarjeta esta activa
+	}
 }

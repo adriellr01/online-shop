ee2f526b443014b7c50cf9c166591d501a603c08
Adriel Linares Ramos <adriellr01@gmail.com>
2023-03-22 22:01:34 -0400

    Agregando tiendas al proyecto

---
+++ OnlineShop/Data/OnlineShopContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Entities;
+
+namespace OnlineShop.Data
+{
+    public class OnlineShopContext : DbContext
+    {
+        public DbSet<Shop> Shops { get; set; }
+
+        public OnlineShopContext(DbContextOptions<OnlineShopContext> context)
+            : base(context)
+        {
+
+        }
+    }
+}
--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
+
 namespace OnlineShop
 {
     public class Program
@@ -13,6 +16,11 @@ namespace OnlineShop
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.AddDbContext<OnlineShopContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+            });
 
             var app = builder.Build();
 

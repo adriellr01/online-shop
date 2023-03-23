93cd6e1f0a11c990185b7d5f9ba5d1d8c818c197
hp <hp@DESKTOP-OTT32A7>
2023-03-23 23:54:17 +0100

    card

--- OnlineShop/Entities/card.cs
+++ OnlineShop/Entities/card.cs
@@ -4,7 +4,9 @@ public class card
 {
 
 		public int Id { get; set; }
-	public string Name { get; set; }
-	public string Address { get; set; }
+	public int nro { get; set; }
+
+	public int tipoid { get; set; }  //tipo de tarjeta(visa, metropolitano, etc)
+		                             //este tipo es llave foranea 
 }
 }

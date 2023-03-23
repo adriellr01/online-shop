b5ab0404d040439beba07d234e4e328b8c469d93
hp <hp@DESKTOP-OTT32A7>
2023-03-23 03:54:42 +0100

    mi class

--- OnlineShop/Entities/card.cs
+++ OnlineShop/Entities/card.cs
@@ -1,8 +1,10 @@
-﻿using System;
-
+﻿namespace OnlineShop.Entities
+{ 
 public class card
 {
-	public card()
-	{
-	}
+
+		public int Id { get; set; }
+	public string Name { get; set; }
+	public string Address { get; set; }
+}
 }

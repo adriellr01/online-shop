e4ad5ee8c60b67cb07473614610aa1e31a27cc44
Sandry <Sandry@DESKTOP-HJC5AM0>
2023-03-23 21:42:30 -0400

    Agregando Producto al Proyecto

--- OnlineShop/Data/OnlineShopContext.cs
+++ OnlineShop/Data/OnlineShopContext.cs
@@ -6,11 +6,12 @@ namespace OnlineShop.Data
     public class OnlineShopContext : DbContext
     {
         public DbSet<Shop> Shops { get; set; }
-
+        public DbSet<Product> Products { get; set; }
         public OnlineShopContext(DbContextOptions<OnlineShopContext> context)
             : base(context)
         {
 
         }
+        
     }
 }
---
+++ OnlineShop/Entities/Product.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+
+namespace OnlineShop.Entities
+{
+    public class Product
+    {
+        [Required]
+        public long Id { get; set; }
+        [Required]
+        public int CategoryId { get; set; }
+        [Required]
+        public int ShopId { get; set; }
+        [Required, MaxLength(256)] 
+        public string Name { get; set; }        
+        [Required, MaxLength(328)] 
+        public string Description { get; set; }
+        [Required, MaxLength(256)] 
+        public string ImagePath { get; set; }
+        [Required]
+        public long Price { get; set; }
+        [Required]
+        public int SoldQuantity { get; set; }
+        [Required]
+        public bool IsActive { get; set; }
+
+  
+        
+
+    }
+}

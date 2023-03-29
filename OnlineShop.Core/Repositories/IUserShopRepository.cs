558774038242cec8919a72861b0b6a158c06d3b6
lcrod <44011097+lcrod@users.noreply.github.com>
2023-03-29 17:50:52 -0400

    Adding IUserRepository and UserRepository, Updating UserControler

---
+++ OnlineShop.Core/Repositories/IUserShopRepository.cs
@@ -0,0 +1,18 @@
+﻿using OnlineShop.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineShop.Core.Repositories
+{
+    public interface IUserShopRepository
+    {
+        List<User> GetAll();
+        User GetById(long id);
+        User Insert(User user);
+        User Update(User user);
+        void Delete(long id);
+    }
+}
---
+++ OnlineShop.EntityFramework/Repositories/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineShop.Core.Repositories;
+using OnlineShop.Core.Entities;
+
+
+namespace OnlineShop.EntityFramework.Repositories
+{
+    public class UserRepository : IUserShopRepository
+    {   
+        public readonly OnlineShopContext _context;
+        public UserRepository(OnlineShopContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(long id)
+        {
+            var delete_user = _context.Users.Where(u => u.id == id).FirstOrDefault();
+            _context.Remove(delete_user);
+            _context.SaveChanges();
+        }
+
+        public List<User> GetAll()
+        {
+            return _context.Users.ToList();
+        }
+
+        public User GetById(long id)
+        {
+            return _context.Users.FirstOrDefault(u => u.id == id);
+        }
+
+        public User Insert(User user)
+        {
+            _context.Add(user);
+            _context.SaveChanges();
+
+            return user;
+        }
+
+        public User Update(User user)
+        {
+            _context.Update(user);
+            _context.SaveChanges(true);
+
+            return user;
+
+        }
+    }
+}

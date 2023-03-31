4b1e348afe9d09efada4eb3a130db87a51ca2fef
anderssonpf <anderssonpf@gmail.com>
2023-03-31 19:28:28 -0400

    subiendo clases andy y danae

--- OnlineShop.Core/Entities/Customer.cs
+++ OnlineShop.Core/Entities/Customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,25 @@ namespace OnlineShop.Core.Entities
 {
     internal class Customer
     {
+        [Required]
+        public long Id { get; set; }
+
+        [MaxLength(256)]
+        public string FullName { get; set; }
+
+        [MaxLength(256)]
+        public string DeviceId { get; set; }
+
+        public long UserId { get; set; }
+
+        [MaxLength(64)]
+        public string Identification { get; set; }
+       
+        [Required, MaxLength(256)]
+        public string EmailAdress { get; set; }
+
+        [MaxLength(32)]
+        public string PhoneNumber { get; set; }
+
     }
 }
